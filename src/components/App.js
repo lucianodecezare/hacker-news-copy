82b0d5fc28c844aeee3fd4d3d2303384362d3630
Luciano de Cezare <lucianodecezare@gmail.com>
2019-11-28 22:39:23 -0300

    Added Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Header from './Header';
+
+import CreateLink from './Link/CreateLink';
+import ForgotPassword from './Auth/ForgotPassword';
+import LinkDetail from './Link/LinkDetail';
+import LinkList from './Link/LinkList';
+import Login from './Auth/Login';
+import SearchLinks from './Link/SearchLinks';
 
 function App() {
   return (
-    <div style={{ margin: "0 auto", textAlign: "center" }}>
-      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <Header />
+        <div className="route-container">
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+            <Route path="/create" component={CreateLink} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot" component={ForgotPassword} />
+            <Route path="/search" component={SearchLinks} />
+            <Route path="/top" component={LinkList} />
+            <Route path="/new/:page" component={LinkList} />
+            <Route path="/link/:linkId" component={LinkDetail} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
