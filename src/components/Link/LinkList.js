8d1e0c417cc8026187764e725c23028576882433
Luciano de Cezare <lucianodecezare@gmail.com>
2019-12-09 19:22:13 -0300

    Added Pagination to LinkList Component

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,47 +1,90 @@
 import React from 'react';
 
 import { FirebaseContext } from '../../firebase/context';
-
 import { LinkItem } from './LinkItem';
+import { LINKS_PER_PAGE } from '../../utils';
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
+  const [cursor, setCursor] = React.useState(null);
+  const isNewPage = props.location.pathname.includes('new');
   const isTopPage = props.location.pathname.includes('top');
+  const page = +props.match.params.page;
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
 
   React.useEffect(() => {
     const unsubscribe = getLinks();
 
     return () => unsubscribe();
-  }, [isTopPage]);
+  }, [isTopPage, page]);
 
   function getLinks() {
+    const hasCursor = Boolean(cursor);
+
     if (isTopPage) {
       return firebase.db
         .collection('links')
         .orderBy('voteCount', 'desc')
+        .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     }
 
-    return firebase.db
-      .collection('links')
-      .orderBy('created', 'desc')
-      .onSnapshot(handleSnapshot);
+    if (page === 1) {
+      return firebase.db
+        .collection('links')
+        .orderBy('created', 'desc')
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    }
+
+    if (hasCursor) {
+      return firebase.db
+        .collection('links')
+        .orderBy('created', 'desc')
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    }
   }
 
   function handleSnapshot(snapshot) {
     const links = snapshot.docs.map((doc) => {
       return { id: doc.id, ...doc.data() };
     });
+    const lastLink = links[links.length - 1];
 
+    setCursor(lastLink);
     setLinks(links);
+  }
+
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  }
+
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
   }
 
   return (
     <div>
       {links.map((link, index) => (
-        <LinkItem key={link.id} showCount={true} link={link} index={index + 1} />
+        <LinkItem key={link.id} showCount={true} link={link} index={index + pageIndex} />
       ))}
+      {isNewPage && (
+        <div class="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
